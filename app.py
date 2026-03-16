324bd07e76d9fbfd5b87cea9762012e563863c30
gauravshivaji <33542486+gauravshivaji@users.noreply.github.com>
2026-03-16 13:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ st.title("📊 Probability Based Stock Prediction Evaluation")
 uploaded_file = st.file_uploader("Upload Dataset", type=["xlsx","csv"])
 
 if uploaded_file is not None:
-    if uploaded_file.name.endswith(".xlsx"):
-        df = pd.read_excel(uploaded_file)
-    else:
-        df = pd.read_csv(uploaded_file)
+
+
+# -----------------------------
+# Load dataset
+# -----------------------------
+if uploaded_file.name.endswith(".xlsx"):
+    df = pd.read_excel(uploaded_file)
+else:
+    df = pd.read_csv(uploaded_file)
 
 # Clean column names
 df.columns = df.columns.astype(str).str.strip()
